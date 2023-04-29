7e58778cda23e2ba2c18159d7784d5631cac9f15
na-br-wo <nathan.b.wood@gmail.com>
2023-04-29 17:57:25 -0600

    added renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'None') {
+    return ''
+  } else if (license === 'GNU GPLv3') {
+    return `![GNU GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (license === 'MIT License') {
+    return `![MIT](https://img.shields.io/badge/License-MIT-blue.svg)`
+  } else if (license === 'Apache License 2.0') {
+    return `![Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
