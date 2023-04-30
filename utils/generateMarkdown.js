e31a5afc2a7e75ed8de7c2c39f36a7b94b0d97fc
na-br-wo <nathan.b.wood@gmail.com>
2023-04-29 18:10:17 -0600

    added the function to generate license markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,7 +24,19 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    // variables for when the other license functions are called
+    const badge = renderLicenseBadge(license)
+    const link = renderLicenseLink(license)
+
+    // return the following markdown code:
+    return `
+    ## License
+    This project is licensed under the ${badge}[${license}](${link}) license.
+    `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
